9436ede46f85e36b1d61288f96b02226716c34d4
Binkozaru <binmy2000@gmail.com>
2023-07-19 16:43:04 +0800

    User input validation, should be done

--- OSAssignment.sh
+++ OSAssignment.sh
@@ -40,16 +40,41 @@ function Registration()
 { 
   echo -e "\t\t\t${BOLD}Patron Registration"
   echo -e "\t\t\t========================"
+  while [[ ${#id} -lt 3 || ! "$id" =~ ^[0-9]+$ ]]; do
+    read -p "Patron ID (As per TARUMT format):" id 
+    if [[ ${#id} -lt 3 || ! "$id" =~ ^[0-9]+$ ]]; then
+      echo "Please enter at least 3 numeric characters."
+    else
+      while [[ -z "$name" || "$name" =~ [^a-zA-Z[:space:]] ]]; do
+          read -r -p "Patron Full Name (As per NRIC) :" name
+          if [[ -z "$name" || "$name" =~ [^a-zA-Z[:space:]] ]]; then
+            echo "Please enter valid name."
+          else
+            while [[ ${#cNumber} -lt 11 || ! "$cNumber" =~ ^[0-9-]+$ ]]; do
+                read -p "Contact Number:" cNumber
+                if [[ ${#cNumber} -lt 11 || ! "$cNumber" =~ ^[0-9-]+$ ]]; then
+                  echo "Please enter valid phone number included '-', e.g 012-3456789"
+                else
+                  while [[ ${#email} -lt 8 || ! "$email" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$ ]]; do
+                      read -p "Email Address (As per TAR UMT format):" email
+                      if [[ ${#email} -lt 8 || ! "$email" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$ ]]; then
+                        echo "Please enter valid email address."
+                      else
+                        filename="Patron.txt"
+                        echo "$id:$name:$cNumber:$email" >> $filename
+                        ContinueRegistration
+                      fi
+                    done
+                fi
 
-  read -p "Patron ID (As per TARUMT format):" id 
-  read -p "Patron Full Name (As per NRIC) :" name
-  read -p "Contact Number:" cNumber
-  read -p "Email Address (As per TAR UMT format):" email
+              done
+          fi
 
-  filename="Patron.txt"
-  echo "$id:$name:$cNumber:$email" >> $filename
+        done
+    fi
+  done
 
-  ContinueRegistration
+  
 }
 
 function ContinueRegistration()
@@ -143,32 +168,62 @@ function AddNewVenue()
   echo -e "\t\t================"
 
   # Get user input
-  read -p "Block Name: " blockName
-  read -p "Room Number: " roomNumber
-  read -p "Room Type: " roomType
-  read -p "Capacity: " capacity
-  read -p "Remarks: " remarks
+  while [[ -z "$blockName" || ! "$blockName" =~ ^[a-zA-Z]+$ ]]; do
+    read -p "Block Name: " blockName
+    if [[ ! "$blockName" =~ ^[a-zA-Z]+$ ]]; then
+      echo "Please enter a valid block"
+    else
+      while [[ -z "$roomNumber" || ! "$roomNumber" =~ ^[a-zA-Z0-9]+$ ]]; do
+        read -p "Room Number: " roomNumber
+        if [[ -z "$roomNumber" || ! "$roomNumber" =~ ^[a-zA-Z0-9]+$ ]]; then
+          echo "Please enter a valid room number"
+        else
+          while [[ -z "$roomType" || ! "$roomType" =~ ^[a-zA-Z]+$ ]]; do
+            read -p "Room Type: " roomType
+            if [[ -z "$roomType" || ! "$roomType" =~ ^[a-zA-Z]+$ ]]; then
+              echo "Please enter a valid room type"
+            else
+              while [[ -z "$capacity" || ! "$capacity" =~ ^[0-9]+$ ]]; do
+                read -p "Capacity: " capacity
+                if [[ -z "$capacity" || ! "$capacity" =~ ^[0-9]+$ ]]; then
+                  echo "Please enter the capacity"
+                else
+                  while [[ -z "$remarks" || ! "$remarks" =~ [^0-9[:space:]] ]]; do
+                    read -p "Remarks: " remarks
+                    if [[ -z "$remarks" || ! "$remarks" =~ [^0-9[:space:]] ]]; then
+                      echo "Please enter proper remarks"
+                    else
+                      status="Available"
 
-  status="Available"
+                      filename="venue.txt"
+                      echo "$blockName:$roomNumber:$roomType:$capacity:$remarks" >> $filename
 
-  filename="venue.txt"
-  echo "$blockName:$roomNumber:$roomType:$capacity:$remarks" >> $filename
+                      # Print out result
+                      echo "Block Name: $blockName"
+                      echo "Room Number: $roomNumber"
+                      echo "Room Type: $roomType"
+                      echo "Capacity: $capacity"
+                      echo "Remarks: $remarks"
+                      echo "Status: $status"
 
-  # Print out result
-  echo "Block Name: $blockName"
-  echo "Room Number: $roomNumber"
-  echo "Room Type: $roomType"
-  echo "Capacity: $capacity"
-  echo "Remarks: $remarks"
-  echo "Status: $status"
-
-  ContinueAddNewVenue
+                      ContinueAddNewVenue
+                    fi
+                  done
+                fi
+              done
+            fi
+          done
+        fi
+      done
+    fi
+  done
+  
 }
 
 function ContinueAddNewVenue()
 {
-  echo "\nAdd Another New Venue? (y)es or (q)uit :"
-  echo "\nPress (q) to return to ${BOLD}University Venue Management Menu."
+  echo "Add Another New Venue? (y)es or (q)uit :"
+  echo "Press (q) to return to ${BOLD}University Venue Management Menu."
   read option
 
   # If the user enters y, then loop back to the beginning
@@ -205,7 +260,7 @@ function ListVenue()
         roomType=$(echo $line | cut -d ":" -f 3)
         capacity=$(echo $line | cut -d ":" -f 4)
         remarks=$(echo $line | cut -d ":" -f 5)
-        echo -e "$roomNumber \t\t $roomType \t\t $capacity \t\t $remarks\t\t\t"
+        echo -e "$roomNumber \t\t $roomType \t\t $capacity \t\t $remarks\t\t\t $status"
     fi
   done < "$filename"
 
@@ -236,7 +291,7 @@ function ContinueListVenue()
 function BookVenue()
 { 
   PatronDetailValidation
-  ContinueBookVenue
+  # ContinueBookVenue
 }
 
 function ContinueBookVenue()
@@ -263,32 +318,39 @@ function PatronDetailValidation()
 
   echo -e "\t\t\tPatron Detail Validation"
   echo -e "\t\t\t========================"
+  echo -e "Please enter the Patron's ID Number" 
+  while [[ ${#id} -lt 3 || ! "$id" =~ ^[0-9]+$ ]]; do
+    read id
+    if [[ ${#id} -lt 3 || ! "$id" =~ ^[0-9]+$ ]]; then
+      echo -e "Please enter valid Patron's ID Number"
+    else
+      filename="Patron.txt"
+      patrons=$(cat $filename)
 
-  echo -e "Please enter the Patron's ID Number"
-  read id
-  
-  filename="Patron.txt"
-  patrons=$(cat $filename)
+      while IFS='' read -r line; do
+        patronID="$(echo $line | cut -d ":" -f 1)"
+        if [ $patronID == $id ]; then
+            valid=true
+            patronName="$(echo $line | cut -d ":" -f 2)"
+            phoneNumber=$(echo $line | cut -d ":" -f 3)
+            email=$(echo $line | cut -d ":" -f 4)
+        fi
+      done < "$filename"
 
-  while IFS='' read -r line; do
-    patronID="$(echo $line | cut -d ":" -f 1)"
-    if [ $patronID == $id ]; then
-        valid=true
-        patronName="$(echo $line | cut -d ":" -f 2)"
-        phoneNumber=$(echo $line | cut -d ":" -f 3)
-        email=$(echo $line | cut -d ":" -f 4)
+      if [ $valid == true ]; then
+        echo -e "\nPatron Name (auto display): $patronName"
+        echo -e "\nPhone Number (auto display): $phoneNumber"
+        echo -e "\nEmail(auto display): $email"
+        ContinueBookVenue
+      else
+        clear
+        echo -e "\nInvalid Input. Please try again\n"
+        id=""
+        BookVenue
+      fi
     fi
-  done < "$filename"
-
-  if [ $valid == true ]; then
-    echo -e "\nPatron Name (auto display): $patronName"
-    echo -e "\nPhone Number (auto display): $phoneNumber"
-    echo -e "\nEmail(auto display): $email"
-  else
-    clear
-    echo -e "\nInvalid Input. Please try again\n"
-    PatronDetailValidation
-  fi
+  done
+  
 }
 
 function BookVenueScreen()
@@ -298,20 +360,27 @@ function BookVenueScreen()
     echo -e "\t\t\tBooking Venue"
     echo -e "\t\t\t============="
     echo -e "\nPlease enter the Room Number : B001A: "
-    read roomNum
+    roomNum=""
+    while [[ ${#roomNum} -lt 4 || ! "$roomNum" =~ ^[a-zA-Z0-9]+$ ]]; do
+      read roomNum
+      if [[ ${#roomNum} -lt 4 || ! "$roomNum" =~ ^[a-zA-Z0-9]+$ ]]; then
+        echo -e "Please enter a valid room number."
+      else
 
-    filename="venue.txt"
+        filename="venue.txt"
+        while IFS='' read -r line; do
+          roomNums="$(echo $line | cut -d ":" -f 2)"
+          if [ $roomNums == $roomNum ]; then
+              valid=true  
+              roomType=$(echo $line | cut -d ":" -f 3)
+              capacity=$(echo $line | cut -d ":" -f 4)
+              remarks=$(echo $line | cut -d ":" -f 5)
 
-    while IFS='' read -r line; do
-      roomNums="$(echo $line | cut -d ":" -f 2)"
-      if [ $roomNums == $roomNum ]; then
-          valid=true  
-          roomType=$(echo $line | cut -d ":" -f 3)
-          capacity=$(echo $line | cut -d ":" -f 4)
-          remarks=$(echo $line | cut -d ":" -f 5)
+          fi
+        done < "$filename"
 
       fi
-    done < "$filename"
+    done
 
     if [ $valid == true ]; then
       echo -e "\nRoom Type (auto display): $roomType"
@@ -324,19 +393,122 @@ function BookVenueScreen()
       BookVenueScreen
     fi
 
-    echo -e "\nNotes: The booking hours shall be from 8am to 8pm only. The booking duration shall be at least 30 minutes per booking."
-    echo -e "\nPlease ${bold}enter${normal} the following details:\n"
+    BookingForDateTime
+    echo "Done For booking date time"
+    if [[ durationValid==true ]]; then
+      echo -e "Reasons for Booking: "
 
-    echo -e "Booking Date (mm/dd/yyyy):"
+      while [[ -z "$reasonBooking" || ! "$reasonBooking" =~ ^[a-zA-Z[:space:]]*$ ]]; do
+        read reasonBooking
+        if [[ -z "$reasonBooking" || ! "$reasonBooking" =~ ^[a-zA-Z[:space:]]*$ ]]; then
+          echo "Please enter proper reason for booking"
+        else
+          ConfirmBooking
+        fi
+      done
+    fi
+}
+
+function BookingForDateTime(){
+  echo -e "\nNotes: The booking hours shall be from 8am to 8pm only. The booking duration shall be at least 30 minutes per booking."
+  echo -e "\nPlease ${bold}enter${normal} the following details:\n"
+
+  echo -e "Booking Date (mm/dd/yyyy):"
+  bookingDate=""
+  while [[ -z "$bookingDate" || ! "$bookingDate" =~ ^[0-9]{2}/[0-9]{2}/[0-9]{4}$ ]]; do
     read bookingDate
-    echo -e "Time From (hh:mm): "
-    read timeFrom
-    echo -e "Time To (hh:mm): "
-    read timeTo
-    echo -e "Reasons for Booking: "
-    read reasonBooking
 
-    ConfirmBooking
+    if [[ -z "$bookingDate" || ! "$bookingDate" =~ ^[0-9]{2}/[0-9]{2}/[0-9]{4}$ ]]; then
+      clear
+      echo "Please enter a valid booking date."
+      BookingForDateTime
+    else
+      month=${bookingDate:0:2}
+      day=${bookingDate:3:2}
+      year=${bookingDate:6:4}
+
+      if [[ $month > 12 || $day > 31 ]]; then
+        clear
+        echo "Please enter a valid date"
+        BookingForDate
+      else
+        if [[ $month == 04 || $month = 06 || $month == 09 || $month == 11 && $day > 30 ]]; then
+          clear
+          echo "Month of "$month" do not have "$day" days."
+          BookingForDate
+        else
+          timeFrom=""
+          echo -e "Time From (hh:mm): "
+          while [[ -z "$timeFrom" || ! "$timeFrom" =~ ^[0-9]{2}:[0-9]{2}$ ]]; do
+            read timeFrom
+            if [[ -z "$timeFrom" || ! "$timeFrom" =~ ^[0-9]{2}:[0-9]{2}$ ]]; then
+              echo "Please enter valid time"
+            else
+              Fromtime_hour=${timeFrom:0:2}
+              Fromtime_min=${timeFrom:3:2}
+              if [[ $Fromtime_hour > 23 || $Fromtime_min > 59 ]]; then
+                clear
+                echo "Please enter valid time"
+                BookingForDateTime
+              else
+                if [[ (08 > $Fromtime_hour) ]]; then
+                  clear
+                  echo $Fromtime_hour
+                  echo "The booking hours shall be from 8am to 8pm only"
+                  BookingForDateTime
+                elif [[ ($Fromtime_hour > 20) ]]; then
+                  clear
+                  echo "The booking hours shall be from 8am to 8pm only"
+                  BookingForDateTime
+                else
+                  durationValid=false
+                  while [[ $durationValid == false ]]; do
+                    durationValid=true
+                    timeTo=""
+                    echo -e "Time To (hh:mm): "
+                    while [[ -z "$timeTo" || ! "$timeTo" =~ ^[0-9]{2}:[0-9]{2}$ ]]; do
+                      read timeTo
+                      if [[ -z "$timeTo" || ! "$timeTo" =~ ^[0-9]{2}:[0-9]{2}$ ]]; then
+                        echo "Please enter valid time"
+                      else
+                        Totime_hour=${timeTo:0:2}
+                        Totime_min=${timeTo:3:2}
+
+                        if [[ $Totime_hour > 23 || $Totime_min > 59 ]]; then
+                          clear
+                          echo "Please enter valid time"
+                          durationValid=false
+                        else
+                          if [[ $((Totime_hour - Fromtime_hour)) == 0 ]]; then
+                              echo "You must book at least 30 minutes"  
+                            if [[ $((Totime_min - Fromtime_min)) < 30 ]]; then
+                              echo "You must book at least 30 minutes"
+                              durationValid=false
+                            else
+                              durationValid=true
+                            fi
+                          else
+                            durationValid=true
+                          fi
+
+                        fi
+
+                      fi
+                      break
+                    done
+                    
+                  done
+                  
+                fi
+              fi
+
+            fi
+          done
+        fi
+      fi  
+    fi
+
+  done
 }
 
 function ConfirmBooking()
@@ -383,5 +555,4 @@ function DisplayReceipt(){
   echo -e "\n\n\t\t\t${bold}Printed on "$current_date" "$current_time"."
 
 }
-
 Main
